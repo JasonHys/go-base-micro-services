2c15e59dcb97768ee90d680d4d19a61ef79fad0c
hys <huangyusheng0660@gmail.com>
2024-08-24 17:16:21 +0800

    kafka

--- kafka/main.go
+++ kafka/main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+)
+
+type Config struct {
+	Kafka struct {
+		Brokers []string "mapstructure:'brokers'"
+		Topic   string   "mapstructure:'topic'"
+	}
+
+	Log struct {
+		Filepath string "mapstructure:'filepath'"
+	}
+}
 
 func main() {
-	fmt.Print("Hello World")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
 
+	//var config Config
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file, %s", err)
+	}
+
+	//if err := viper.Unmarshal(&config); err != nil {
+	//	log.Fatalf("can not parse config file, %s", err)
+	//}
+	//
+	//fmt.Printf("%#v", config)
+
+	// kafka配置
+	brokers := viper.GetStringSlice("kafka.brokers")
+	topic := viper.GetString("kafka.topic")
+
+	conf :=
 }
